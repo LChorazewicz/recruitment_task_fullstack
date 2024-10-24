35f566b8a218ff972d3921b6dcec6e19ca25540e
LChorazewicz <leszek.chorazewicz@gmail.com>
2024-10-24 13:32:12 +0200

    logger added; some changes

--- src/App/ExchangeRates/Infrastructure/Adapter/NBPExchangeRateAdapter.php
+++ src/App/ExchangeRates/Infrastructure/Adapter/NBPExchangeRateAdapter.php
@@ -5,24 +5,37 @@ namespace App\ExchangeRates\Infrastructure\Adapter;
 use App\ExchangeRates\Domain\Port\ExchangeRate;
 use App\ExchangeRates\Domain\Port\ExchangeRateProvider;
 use App\ExchangeRates\Domain\Port\Filter;
+use App\ExchangeRates\Infrastructure\Logger\Logger;
 
 class NBPExchangeRateAdapter implements ExchangeRateProvider
 {
+    private $logger;
+
+    public function __construct(Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /** @return ExchangeRate[] */
     public function fetch(Filter $filter): array
     {
         $baseUrl = 'https://api.nbp.pl/api/exchangerates';
         $url = sprintf('%s/tables/A/%s?format=json', $baseUrl, $filter->getDate()->format('Y-m-d'));
 
-        $response = json_decode(file_get_contents($url), true)[0];
+        $rates = [];
+        try {
+            $response = json_decode(file_get_contents($url), true)[0];
 
-        $response['rates'] = array_filter($response['rates'], function (array $rate) use ($filter) {
-            return in_array($rate['code'], $filter->getCurrencies());
-        });
+            $rates = array_filter($response['rates'], function (array $rate) use ($filter) {
+                return in_array($rate['code'], $filter->getCurrencies());
+            });
+        } catch (\Exception $exception) {
+            $this->logger->log($exception);
+        }
 
         $result = [];
 
-        foreach ($response['rates'] as $item) {
+        foreach ($rates as $item) {
             $result[] = new ExchangeRate(
                 $item['code'],
                 $item['currency'],
---
+++ src/App/ExchangeRates/Infrastructure/Logger/Logger.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\ExchangeRates\Infrastructure\Logger;
+
+class Logger
+{
+    public function __construct() {}
+
+    public function log(string $log): void {}
+}
